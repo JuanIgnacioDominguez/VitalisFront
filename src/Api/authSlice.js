dc344e28c6e5405fa930a7f16dc070a7b14cbc75
ThomasGiardina <thomasgiardina0@gmail.com>
2025-05-30 10:44:53 -0300

    a

--- src/Api/authSlice.js
+++ src/Api/authSlice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-// Thunk para registrar usuario usando axios
 export const registerUser = createAsyncThunk(
     'auth/registerUser',
     async (userData, { rejectWithValue }) => {
