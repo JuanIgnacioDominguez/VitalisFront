5ea99178354f77296d87834b3d3d4068a07a1155
juanchy <juanigdominguez03@gmail.com>
2025-05-30 10:46:33 -0300

    safe area nav

--- src/components/BotomNavbar/BottomNavbar.jsx
+++ src/components/BotomNavbar/BottomNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, SafeAreaView } from 'react-native'
 import {
     HomeIcon,
     CalendarDaysIcon,
@@ -12,27 +12,29 @@ const tabs = [
     { label: 'Menu', icon: HomeIcon, route: 'Home' },
     { label: 'Turnos', icon: CalendarDaysIcon, route: 'Appointments' },
     { label: 'Favoritos', icon: HeartIcon, route: 'Favorites' },
-    { label: 'Perfil', icon: UserIcon, route: 'User' } 
+    { label: 'Perfil', icon: UserIcon, route: 'User' }
 ]
 
 export default function BottomNavbar() {
     const navigation = useNavigation()
 
     return (
-        <View className="absolute bottom-0 left-0 right-0 bg-primary-light flex-row justify-between px-2 py-1 h-[56px] border-t border-quaternary-light">
-        {tabs.map((tab, idx) => (
-            <TouchableOpacity
-                key={tab.label}
-                className="flex-1 items-center justify-center"
-                activeOpacity={0.7}
-                onPress={() => {
-                    if (tab.route) navigation.navigate(tab.route)
-                }}
-            >
-                <tab.icon size={24} color="#FFFFFF" />
-                <Text className="text-white text-xs mt-1">{tab.label}</Text>
-            </TouchableOpacity>
-        ))}
-        </View>
+        <SafeAreaView>
+            <View className="bg-primary-light flex-row justify-between px-2 py-1 h-[56px] border-t border-quaternary-light">
+                {tabs.map((tab, idx) => (
+                    <TouchableOpacity
+                        key={tab.label}
+                        className="flex-1 items-center justify-center"
+                        activeOpacity={0.7}
+                        onPress={() => {
+                            if (tab.route) navigation.navigate(tab.route)
+                        }}
+                    >
+                        <tab.icon size={24} color="#FFFFFF" />
+                        <Text className="text-white text-xs mt-1">{tab.label}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+        </SafeAreaView>
     )
 }
